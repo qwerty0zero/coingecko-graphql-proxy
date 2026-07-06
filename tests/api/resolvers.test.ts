dd875b33d138e9bcd8fddb11bc60489e067fda2e
qwerty0zero <igoricloud98@gmail.com>
2026-07-06 23:50:12 +0200

    chore: remove tests

--- tests/api/resolvers.test.ts
+++
@@ -1,7 +0,0 @@
-import { resolvers } from '../../src/api/resolvers.js';
-
-describe('Resolvers', () => {
-  it('should export Query.historicalPrices', () => {
-    expect(typeof resolvers.Query.historicalPrices).toBe('function');
-  });
-});
